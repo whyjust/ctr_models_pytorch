2550b255818a0321a22bda15268ed05b0772742a
whyjust <1677234597@qq.com>
2022-03-14 23:27:44 +0800

    add layer

--- deepCTR/layers/interaction.py
+++ deepCTR/layers/interaction.py
@@ -6,6 +6,7 @@
 import os
 import sys
 import itertools
+from turtle import forward
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,9 +18,9 @@ from sequence import DynamicGRU, KMaxPooling
 class FM(nn.Module):
     """
     因子分解机-二维交叉特征
-    输入形状：
+    输入形状:
         - 3维矩阵 (batch_size, field_size, embedding_size)
-    输出矩阵：
+    输出矩阵:
         - 2维矩阵 (batch_size, 1)
     References
         - [Factorization Machines](https://www.csie.ntu.edu.tw/~b97053/paper/Rendle2010FM.pdf)
@@ -43,11 +44,11 @@ class BiInteractionPooling(nn.Module):
     """
      Bi-Interaction Layer used in Neural FM,compress the
      pairwise element-wise product of features into one single vector.
-     神经FM中使用的双向交互层，将特征的成对元素乘积压缩为一个向量
+     神经FM中使用的双向交互层,将特征的成对元素乘积压缩为一个向量
 
-     输入形状：
-        - 3维矩阵，(batch_size, field_size, embedding_size)
-     输出形状：
+     输入形状:
+        - 3维矩阵,(batch_size, field_size, embedding_size)
+     输出形状:
         - 3维矩阵, (batch_size, 1, embedding_size)
      References
         - [He X, Chua T S. Neural factorization machines for sparse predictive analytics[C]//Proceedings of the 40th International ACM SIGIR conference on Research and Development in Information Retrieval. ACM, 2017: 355-364.](http://arxiv.org/abs/1708.05027)
@@ -67,9 +68,9 @@ class BiInteractionPooling(nn.Module):
 class SENETLayer(nn.Module):
     """
     应用于FiBiNET中的SENETLayer
-    输出形状：
+    输出形状:
         - 3维数组, (batch_size, field_size, embedding_size)
-    输入形状：
+    输入形状:
         - 3维数组, (batch_size, field_size, embedding_size)
     """
     def __init__(self, field_size, reduction_ratio=3, seed=1024, device='cpu'):
@@ -101,17 +102,17 @@ class SENETLayer(nn.Module):
         A = self.excitation(Z)
         # inputs (batch_size,field_size,embedding_size)
         # torch.unsqueeze(A, dim=2) -> (batch_size,field_size,1)
-        # 输出output形状：(batch_size,filed_size,embedding_size)
+        # 输出output形状:(batch_size,filed_size,embedding_size)
         V = torch.mul(inputs, torch.unsqueeze(A, dim=2))
         return V
 
 class BilinearInteraction(nn.Module):
     """
     BilinearInteraction Layer used in FiBiNET.
-    输入形状：
-        - 3维数组，(batch_size,field_size, embedding_size)
-    输出形状：
-        - 3维数组，(batch_size,field_size*(field_size-1)/2, embedding_size)
+    输入形状:
+        - 3维数组,(batch_size,field_size, embedding_size)
+    输出形状:
+        - 3维数组,(batch_size,field_size*(field_size-1)/2, embedding_size)
     """
 
     def __init__(self, field_size, embedding_size, bilinear_type="interaction", seed=1024, device='cpu'):
@@ -158,10 +159,10 @@ class BilinearInteraction(nn.Module):
 class CIN(nn.Module):
     """
     xDeepFM中使用的压缩交互网络
-    输入形状：
-        - 3维矩阵， (batch_size,field_size,embedding_size)`
-    输出形状：
-        - 2维矩阵， (batch_size, featuremap_num)
+    输入形状:
+        - 3维矩阵, (batch_size,field_size,embedding_size)`
+    输出形状:
+        - 2维矩阵, (batch_size, featuremap_num)
         - featuremap_num =  sum(self.layer_size[:-1]) // 2 + self.layer_size[-1]`` if ``split_half=True``,else  ``sum(layer_size)
     References
         - [Lian J, Zhou X, Zhang F, et al. xDeepFM: Combining Explicit and Implicit Feature Interactions for Recommender Systems[J]. arXiv preprint arXiv:1803.05170, 2018.] (https://arxiv.org/pdf/1803.05170.pdf)
@@ -171,7 +172,7 @@ class CIN(nn.Module):
         :param field_size: 特征组的数量
         :param layer_size: list of int, 每一层的特征映射
         :param activation: 激活函数
-        :param split_half: bool,如果设置为False，每个隐藏的特征映射的一半将连接到输出单元
+        :param split_half: bool,如果设置为False,每个隐藏的特征映射的一半将连接到输出单元
         :param l2_reg:
         :param seed:
         :param device:
@@ -198,7 +199,7 @@ class CIN(nn.Module):
             )
 
             if self.split_half:
-                # 如果split_half=True，则layer_size必须为偶数
+                # 如果split_half=True,则layer_size必须为偶数
                 if i != len(self.layer_size) - 1 and size % 2 > 0:
                     raise ValueError(
                         "layer_size must be even number except for the last layer when split_half=True")
@@ -254,17 +255,17 @@ class CIN(nn.Module):
             final_result.append(direct_connect)
             hidden_nn_layers.append(next_hidden)
         result = torch.cat(final_result, dim=1)
-        # (batch_size, featuremap_num)，dim=-1则合并完成
+        # (batch_size, featuremap_num),dim=-1则合并完成
         result = torch.sum(result, -1)
         return result
 
 class AFMLayer(nn.Module):
     """
     注意力因子分解机对没有线性项和偏差的成对（2 阶）特征交互进行建模
-    输入形状：
-        - 3维数组， (batch_size, 1, embedding_size)
-    输出形状：
-        - 2维数组， (bacth_size, 1)
+    输入形状:
+        - 3维数组, (batch_size, 1, embedding_size)
+    输出形状:
+        - 2维数组, (bacth_size, 1)
     References
         - [Attentional Factorization Machines : Learning the Weight of Feature
         Interactions via Attention Networks](https://arxiv.org/pdf/1708.04617.pdf)
@@ -306,7 +307,7 @@ class AFMLayer(nn.Module):
         embeds_vec_list = inputs
         row = []
         col = []
-        # itertools.combinations实现排列组合，但是会自动过滤重复值
+        # itertools.combinations实现排列组合,但是会自动过滤重复值
         for r,c in itertools.combinations(embeds_vec_list, 2):
             row.append(r)
             row.append(c)
@@ -336,10 +337,10 @@ class AFMLayer(nn.Module):
 class InteractingLayer(nn.Module):
     """
     AutoInt Layer: 通过多头注意力机制的不同领域的相关性模型
-    输入形状：
-        - 3维数组，形状为(batch_size, field_size, embedding_size)
-    输出形状：
-        - 3维数组，形状为(batch_size, field_size, embedding_size)
+    输入形状:
+        - 3维数组,形状为(batch_size, field_size, embedding_size)
+    输出形状:
+        - 3维数组,形状为(batch_size, field_size, embedding_size)
     References
         - [Song W, Shi C, Xiao Z, et al. AutoInt: Automatic Feature Interaction Learning via Self-Attentive Neural Networks[J]. arXiv preprint arXiv:1810.11921, 2018.](https://arxiv.org/abs/1810.11921)
     """
@@ -379,7 +380,7 @@ class InteractingLayer(nn.Module):
             raise ValueError(
                 "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(inputs.shape)))
 
-        # inputs作为输入，分别与权重矩阵W_query、W_key、W_value点乘
+        # inputs作为输入,分别与权重矩阵W_query、W_key、W_value点乘
         # 指定inputs为dim=-1, 权重dim=0进行点乘 如 (2,1,3) dot (3,5,2) - > (2,1,5,2)
         # (None, F, D)
         querys = torch.tensordot(inputs, self.W_query, dims=([-1], [0]))
@@ -387,7 +388,7 @@ class InteractingLayer(nn.Module):
         values = torch.tensordot(inputs, self.W_value, dims=([-1],[0]))
 
         # 按照multi_head中head_num进行切割
-        # 先按照dim=2进行均分，然后进行合并，例如torch.split(6,4,4, dim=2)->2个(6,4,2)进行stack->(2,6,4,2)
+        # 先按照dim=2进行均分,然后进行合并,例如torch.split(6,4,4, dim=2)->2个(6,4,2)进行stack->(2,6,4,2)
         # (head_num, None, F, D//head_num)
         querys = torch.stack(torch.split(querys, self.att_embedding_size, dim=2))
         keys = torch.stack(torch.split(keys, self.att_embedding_size, dim=2))
@@ -412,9 +413,9 @@ class InteractingLayer(nn.Module):
 class CrossNet(nn.Module):
     """
     Deep&Cross Network模型的Cross Network部分 它倾向于低度和高度交叉特征
-    输入形状：
+    输入形状:
         - 2维数组, (batch_size, units)
-    输出形状：
+    输出形状:
         - 2维数组, (batch_size, units)
 
     References
@@ -466,9 +467,193 @@ class CrossNet(nn.Module):
         x_l = torch.squeeze(x_l, dim=2)
         return x_l
 
+class CrossNetMix(nn.Module):
+    """
+    Cross Network中的DCN-Mix模型, 对于DCN-M有以下提升:
+    1 添加了MOE用于学习在不同子空间中特征的相互影响
+    2 在低维空间中添加了非线性的转换
+
+    输入形状:
+        - 2维数组, (batch_size, units)
+    输出形状:
+        - 2维数组, (batch_size, units)
+    
+    References
+        - [Wang R, Shivanna R, Cheng D Z, et al. DCN-M: Improved Deep & Cross Network for Feature Cross Learning in Web-scale Learning to Rank Systems[J]. 2020.](https://arxiv.org/abs/2008.13535)
+    """
+    def __init__(self, in_features, low_rank=32, num_experts=4, layer_num=2, device='cpu'):
+        """
+        Args:
+            in_features (_type_): 输入特征的维度
+            low_rank (int, optional): 低维空间中的维度. Defaults to 32.
+            num_experts (int, optional): exports数量. Defaults to 4.
+            layer_num (int, optional): cross layer数量. Defaults to 2.
+            device (str, optional): 运行的设备. Defaults to 'cpu'.
+        """
+        super(CrossNetMix, self).__init__()
+        self.layer_num = layer_num
+        self.num_experts = num_experts
+        
+        # U: (in_features, low_rank)
+        self.U_list = nn.Parameter(torch.Tensor(self.layer_num, num_experts, in_features, low_rank))
+        # V: (in_features, low_rank)
+        self.V_list = nn.Parameter(torch.Tensor(self.layer_num, num_experts, in_features, low_rank))
+        # C: (low_rank, low_rank)
+        self.C_list = nn.Parameter(torch.Tensor(self.layer_num, num_experts, low_rank, low_rank))
+        self.gating = nn.ModuleList([nn.Linear(in_features, 1, bias=False) for i in range(self.num_experts)])
+        self.bias = nn.Parameter(torch.Tensor(self.layer_num, in_features, 1))
+
+        init_para_list = [self.U_list, self.V_list, self.C_list]
+        for para in init_para_list:
+            for i in range(self.layer_num):
+                nn.init.xavier_normal_(para[i])
+
+        for i in range(len(self.bias)):
+            nn.init.zeros_(self.bias[i])
+        
+        self.to(device)
+    
+    def forward(self, inputs):
+        # 维度为(bs, in_features, 1)
+        x_0 = inputs.unsqueeze(2)
+        x_l = x_0
+        for i in range(self.layer_num):
+            output_of_experts = []
+            gating_score_of_experts = []
+            for expert_id in range(self.num_experts):
+                # (1) G(x_l)
+                # compute the gating score by x_l
+                gating_score_of_experts.append(self.gating[expert_id](x_l.squeeze(2)))
+
+                # (2) E(x_l)
+                # project the input x_l to $\mathbb{R}^{r}$
+                v_x = torch.matmul(self.V_list[i][expert_id].t(), x_l)  # (bs, low_rank, 1)
+
+                # nonlinear activation in low rank space
+                v_x = torch.tanh(v_x)
+                v_x = torch.matmul(self.C_list[i][expert_id], v_x)
+                v_x = torch.tanh(v_x)
+
+                # project back to $\mathbb{R}^{d}$
+                uv_x = torch.matmul(self.U_list[i][expert_id], v_x)  # (bs, in_features, 1)
+
+                dot_ = uv_x + self.bias[i]
+                dot_ = x_0 * dot_  # Hadamard-product
+
+                output_of_experts.append(dot_.squeeze(2))
+            # (3) mixture of low-rank experts
+            output_of_experts = torch.stack(output_of_experts, 2)  # (bs, in_features, num_experts)
+            gating_score_of_experts = torch.stack(gating_score_of_experts, 1)  # (bs, num_experts, 1)
+            moe_out = torch.matmul(output_of_experts, gating_score_of_experts.softmax(1))
+            x_l = moe_out + x_l  # (bs, in_features, 1)
+
+        x_l = x_l.squeeze()  # (bs, in_features)
+        return x_l
+
+class InnerProductLayer(nn.Module):
+    """
+    PNN 中使用的 InnerProduct 层, 用于计算特征向量之间的元素乘积或内积
+    输入形状:
+        - 3维数组列表, [(batch_size, 1, embedding_size),...]
+    输出形状:
+        - reduce_sum=True, 3维数组, (batch_size, N*(N-1)/2, 1)
+        - reduce_sum=False, 3维数组, (batch_size, N*(N-1)/2, embedding_size)
+    
+    References
+        - [Qu Y, Cai H, Ren K, et al. Product-based neural networks for user response prediction[C]//
+        Data Mining (ICDM), 2016 IEEE 16th International Conference on. IEEE, 2016: 1149-1154.]
+        (https://arxiv.org/pdf/1611.00144.pdf)
+    """
+    def __init__(self, reduce_sum=True, device='cpu'):
+        super(InnerProductLayer, self).__init__()
+        self.reduce_sum = reduce_sum
+        self.to(device)
+    
+    def forward(self, inputs):
+        embed_list = inputs
+        row = []
+        col = []
+        num_inputs = len(embed_list)
+
+        for i in range(num_inputs - 1):
+            for j in range(i + 1, num_inputs):
+                row.append(i)
+                col.append(j)
+        # size -> (batch, num_pairs, k)
+        p = torch.cat([embed_list[idx] for idx in row], dim=1)
+        q = torch.cat([embed_list[idx] for idx in col], dim=1)
+
+        inner_product = p * q
+        if self.reduce_sum:
+            inner_product = torch.sum(inner_product, dim=2, keepdim=True)
+        return inner_product
 
 
+class OutterProductLayer(nn.Module):
+    """
+    PNN中的OutterProduct Layer
+    输入形状:
+        - 3维数组列表, [(batch_size, 1, embedding_size),...]
+    输出形状:
+        - 2维数组, (batch_size, N*(N-1))
+    References
+        - [Qu Y, Cai H, Ren K, et al. Product-based neural networks for user response prediction[C]//Data Mining (ICDM), 2016 IEEE 16th International Conference on. IEEE, 2016: 1149-1154.](https://arxiv.org/pdf/1611.00144.pdf)
+    """
+    def __init__(self, field_size, embedding_size, kernel_type='mat', seed=1024, device='cpu'):
+        super(OutterProductLayer, self).__init__()
+        self.kernel_type = kernel_type
 
+        num_inputs = field_size
+        num_pairs = int(num_inputs * (num_inputs - 1) / 2)
+        embed_size = embedding_size
+        
+        if self.kernel_type == "mat":
+            self.kernel = nn.Parameter(torch.Tensor(embed_size, num_pairs, embed_size))
+        elif self.kernel_type == "vec":
+            self.kernel = nn.Parameter(torch.Tensor(num_pairs, embed_size))
+        elif self.kernel_type == "num":
+            self.kernel = nn.Parameter(torch.Tensor(num_pairs, 1))
+        
+        nn.init.uniform_(self.kernel)
+        self.to(device)
+    
+    def forward(self, inputs):
+        embed_list = inputs
+        row = []
+        col = []
+        num_inputs = len(embed_list)
+        for i in range(num_inputs - 1):
+            for j in range(i + 1, num_inputs):
+                row.append(i)
+                col.append(j)
+        p = torch.cat([embed_list[idx]
+                       for idx in row], dim=1)  # batch num_pairs k
+        q = torch.cat([embed_list[idx] for idx in col], dim=1)
+        
+        if self.kernel_type == "mat":
+            p.unsqueeze_(dim=1)
+            # k     k* pair* k
+            # batch * pair
+            kp = torch.sum(
+                # batch * pair * k
+                torch.mul(
+                    # batch * pair * k
+                    torch.transpose(
+                        # batch * k * pair
+                        torch.sum(
+                            # batch * k * pair * k
+                            torch.mul(p, self.kernel),
+                        dim=-1),
+                    2, 1),
+                q),
+            dim=-1)
+        else:
+            # 1 * pair * (k or 1)
+            k = torch.unsqueeze(self.kernel, 0)
+            # batch * pair
+            kp = torch.sum(p * q * k, dim=-1)
+            # p q # b * p * k
+        return kp
 
 
 
@@ -510,5 +695,19 @@ if __name__ == '__main__':
     cross_inputs = torch.randn((128, 8))
     print('cross net:', cross_net(cross_inputs).shape)
 
+    # crossMixNet
+    cross_mix_net = CrossNetMix(in_features=8, low_rank=32, num_experts=4, layer_num=2, device='cpu')
+    cross_mix_inputs = torch.randn((128, 8))
+    print('cross mix net:', cross_mix_net(cross_mix_inputs).shape)
 
+    # innerProductLayer
+    inner_product_layer = InnerProductLayer(reduce_sum=False, device='cpu')
+    inner_inputs = [torch.randn((batch_size, 1, embedding_size)) for _i in range(8)]
+    print('inner product layer:', inner_product_layer(inner_inputs).shape)
     
+    # outter_product_layer
+    outter_product_layer = OutterProductLayer(field_size=8, embedding_size=32, kernel_type="mat")
+    outter_inputs = [torch.randn((batch_size, 1, 32)) for _ in range(8)]
+    print("outter product layer:", outter_product_layer(outter_inputs).shape)
+
+
