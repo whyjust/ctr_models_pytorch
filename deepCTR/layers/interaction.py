a600b4c102afc7b4d44466cb85f24d8bee73e756
whyjust <1677234597@qq.com>
2022-03-14 21:36:07 +0800

    add code

--- deepCTR/layers/interaction.py
+++ deepCTR/layers/interaction.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 # @FileName  : interaction.py
@@ -270,7 +269,6 @@ class AFMLayer(nn.Module):
         - [Attentional Factorization Machines : Learning the Weight of Feature
         Interactions via Attention Networks](https://arxiv.org/pdf/1708.04617.pdf)
     """
-
     def __init__(self, in_features, attention_factor=4, l2_reg_w=0, dropout_rate=0, seed=1024, device='cpu'):
         """
         :param in_features: 输入特征维数
@@ -287,9 +285,11 @@ class AFMLayer(nn.Module):
         self.seed = seed
         embedding_size = in_features
 
-        self.attention_W = nn.Parameter(torch.Tensor(embedding_size, self.attention_factor))
+        self.attention_W = nn.Parameter(torch.Tensor(
+            embedding_size, self.attention_factor))
         self.attention_b = nn.Parameter(torch.Tensor(self.attention_factor))
-        self.projection_h = nn.Parameter(torch.Tensor(self.attention_factor, 1))
+        self.projection_h = nn.Parameter(
+            torch.Tensor(self.attention_factor, 1))
         self.projection_p = nn.Parameter(torch.Tensor(embedding_size, 1))
 
         # 初始化为正态分布矩阵
@@ -299,7 +299,7 @@ class AFMLayer(nn.Module):
         for tensor in [self.attention_b]:
             nn.init.zeros_(tensor, )
 
-        self .dropout = nn.Dropout(dropout_rate)
+        self.dropout = nn.Dropout(dropout_rate)
         self.to(device)
 
     def forward(self, inputs):
@@ -333,6 +333,144 @@ class AFMLayer(nn.Module):
             attention_output, self.projection_p, dims=([-1], [0]))
         return afm_out
 
+class InteractingLayer(nn.Module):
+    """
+    AutoInt Layer: 通过多头注意力机制的不同领域的相关性模型
+    输入形状：
+        - 3维数组，形状为(batch_size, field_size, embedding_size)
+    输出形状：
+        - 3维数组，形状为(batch_size, field_size, embedding_size)
+    References
+        - [Song W, Shi C, Xiao Z, et al. AutoInt: Automatic Feature Interaction Learning via Self-Attentive Neural Networks[J]. arXiv preprint arXiv:1810.11921, 2018.](https://arxiv.org/abs/1810.11921)
+    """
+    def __init__(self, embedding_size, head_num=2, use_res=True, scaling=False, seed=1024, device='cpu'):
+        """
+        :param embedding_size: 特征维度
+        :param head_num: 多头注意力机制数量
+        :param use_res:
+        :param scaling:
+        :param seed:
+        :param device:
+        """
+        super(InteractingLayer, self).__init__()
+        if head_num <= 0:
+            raise ValueError('head_num must be a int > 0')
+        if embedding_size % head_num != 0:
+            raise ValueError('embedding_size is not a integer multiple of head_num!')
+        self.att_embedding_size = embedding_size // head_num
+        self.head_num = head_num
+        self.use_res = use_res
+        self.scaling = scaling
+        self.seed = seed
+
+        # 构建query、key与value矩阵
+        self.W_query = nn.Parameter(torch.Tensor(embedding_size, embedding_size))
+        self.W_key = nn.Parameter(torch.Tensor(embedding_size, embedding_size))
+        self.W_value = nn.Parameter(torch.Tensor(embedding_size, embedding_size))
+
+        if self.use_res:
+            self.W_Res = nn.Parameter(torch.Tensor(embedding_size, embedding_size))
+        for tensor in self.parameters():
+            nn.init.normal_(tensor, mean=0.0, std=0.05)
+        self.to(device)
+
+    def forward(self, inputs):
+        if len(inputs.shape) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(inputs.shape)))
+
+        # inputs作为输入，分别与权重矩阵W_query、W_key、W_value点乘
+        # 指定inputs为dim=-1, 权重dim=0进行点乘 如 (2,1,3) dot (3,5,2) - > (2,1,5,2)
+        # (None, F, D)
+        querys = torch.tensordot(inputs, self.W_query, dims=([-1], [0]))
+        keys = torch.tensordot(inputs, self.W_key, dims=([-1], [0]))
+        values = torch.tensordot(inputs, self.W_value, dims=([-1],[0]))
+
+        # 按照multi_head中head_num进行切割
+        # 先按照dim=2进行均分，然后进行合并，例如torch.split(6,4,4, dim=2)->2个(6,4,2)进行stack->(2,6,4,2)
+        # (head_num, None, F, D//head_num)
+        querys = torch.stack(torch.split(querys, self.att_embedding_size, dim=2))
+        keys = torch.stack(torch.split(keys, self.att_embedding_size, dim=2))
+        values = torch.stack(torch.split(values, self.att_embedding_size, dim=2))
+
+        # (head_num, None, F, F)
+        inner_product = torch.einsum('bnik,bnjk->bnij', querys, keys)
+        if self.scaling:
+            inner_product /= self.att_embedding_size ** 0.5
+        # (head_num, None, F, F)
+        self.normalized_att_scores = F.softmax(inner_product, dim=-1)
+        # (head_num, None, F, D/head_num)
+        result = torch.matmul(self.normalized_att_scores, values)
+
+        result = torch.cat(torch.split(result, 1, ), dim=-1)
+        # (None, F, D)
+        result = torch.squeeze(result, dim=0)
+        if self.use_res:
+            result += torch.tensordot(inputs, self.W_Res, dims=([-1], [0]))
+        return result
+
+class CrossNet(nn.Module):
+    """
+    Deep&Cross Network模型的Cross Network部分 它倾向于低度和高度交叉特征
+    输入形状：
+        - 2维数组, (batch_size, units)
+    输出形状：
+        - 2维数组, (batch_size, units)
+
+    References
+        - [Wang R, Fu B, Fu G, et al. Deep & cross network for ad click predictions[C]//Proceedings of the ADKDD'17. ACM, 2017: 12.](https://arxiv.org/abs/1708.05123)
+        - [Wang R, Shivanna R, Cheng D Z, et al. DCN-M: Improved Deep & Cross Network for Feature Cross Learning in Web-scale Learning to Rank Systems[J]. 2020.](https://arxiv.org/abs/2008.13535)
+    """
+    def __init__(self, in_features, layer_num=2, parameterization='vector', seed=1024, device='cpu'):
+        super(CrossNet, self).__init__()
+        self.layer_num = layer_num
+        self.parameterization = parameterization
+        if self.parameterization == 'vector':
+            # weight in DCN (in_features, 1)
+            self.kernels = nn.Parameter(torch.Tensor(self.layer_num, in_features, 1))
+        elif self.parameterization == 'maxtrix':
+            # weight matrix in DCN-M.  (in_features, in_features)
+            self.kernels = nn.Parameter(torch.Tensor(self.layer_num, in_features, in_features))
+        else:
+            raise ValueError("parameterization should be 'vector' or 'matrix'")
+
+        self.bias = nn.Parameter(torch.Tensor(self.layer_num, in_features, 1))
+
+        for i in range(self.kernels.shape[0]):
+            nn.init.xavier_normal_(self.kernels[i])
+        for i in range(self.bias.shape[0]):
+            nn.init.zeros_(self.bias[i])
+
+        self.to(device)
+
+
+    def forward(self, inputs):
+        # inputs -> (batch_size, units)
+        # x_0 = (batch_size, units, 1)
+        x_0 = inputs.unsqueeze(2)
+        x_l = x_0
+        for i in range(self.layer_num):
+            if self.parameterization == 'vector':
+                xl_w = torch.tensordot(x_l, self.kernels[i], dims=([1],[0]))
+                dot_ = torch.matmul(x_0, xl_w)
+                x_l = dot_ + self.bias[i] + x_l
+            elif self.parameterization == 'matrix':
+                # W * xi (in_features, 1)
+                xl_w = torch.matmul(self.kernels[i], x_l)
+                # W * xi + b
+                dot_ = xl_w + self.bias[i]
+                # x0 · (W * xi + b) +xl  Hadamard-product
+                x_l = x_0 * dot_ + x_l
+            else:
+                raise ValueError("parameterization should be 'vector' or 'matrix'")
+        x_l = torch.squeeze(x_l, dim=2)
+        return x_l
+
+
+
+
+
+
 
 if __name__ == '__main__':
     batch_size, field_size, embedding_size = 128, 20, 32
@@ -340,18 +478,37 @@ if __name__ == '__main__':
     # FM
     fm = FM()
     print('fm:',fm(inputs).shape)  # (batch_size, 1)
+
     # BiInteractionPooling
     BIPooling = BiInteractionPooling()
     print('BIPooling:',BIPooling(inputs).shape)  # (batch_size, 1, embedding_size)
+
     # SENETLayer
     senelt_layer = SENETLayer(field_size, reduction_ratio=3, seed=1024, device='cpu')
     print('senelt_layer:',senelt_layer(inputs).shape)   # (batch_size, field_size, embedding_size)
+
     # BilinearInteraction
     bilinear_interaction = BilinearInteraction(field_size, embedding_size, bilinear_type="interaction", seed=1024, device='cpu')
     print('bilinear_interaction:', bilinear_interaction(inputs).shape)  # (batch_size, field_size*(field_size)/2, embedding_size)
+
     # CIN
     cin = CIN(field_size, layer_size=(128, 128), activation='relu', split_half=True, l2_reg=1e-5, seed=1024, device='cpu')
     print('cin:', cin(inputs).shape)  # (batch_size, sum(layer_size[:-1]//2 + self.layer_size[-1]))  128//2 + 128 = 192
-    # AFM layer
-    AFMLayer()
 
+    # # AFM layer
+    # afm_layer = AFMLayer(32, attention_factor=4, l2_reg_w=0)
+    # afm_inputs = torch.randn((batch_size, 1, embedding_size))
+    # print('afm layer:', AFMLayer(afm_inputs).shape)
+
+    # Interaction layer
+    interaction_layer = InteractingLayer(embedding_size, head_num=2, use_res=True, scaling=False, seed=1024, device='cpu')
+    print('interaction layer:', interaction_layer(inputs).shape)
+
+    # crossNet
+    # in_features与Unit需要一致
+    cross_net = CrossNet(in_features=8, layer_num=2, parameterization='vector', seed=1024, device='cpu')
+    cross_inputs = torch.randn((128, 8))
+    print('cross net:', cross_net(cross_inputs).shape)
+
+
+    
